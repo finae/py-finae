28338a50ec01a2d8a6e8930bbe3146f3543ebb3d
Ning Ren <renning22@gmail.com>
2023-11-25 01:40:52 -0800

    negative

--- experiment/confirmative.py
+++ experiment/confirmative.py
@@ -1,8 +1,16 @@
 from finae import ask_llm
 
-
 output = ask_llm('''
 Answer: Yes, red is a color. It is one of the primary colors and is often associated with emotions such as love, passion, and anger.
+                 
+Is it a confirmative answer or not?
+''')
+
+print(output)
+
+
+output = ask_llm('''
+Answer: Yes, it is a confirmative answer. It is a statement that affirms that red is a color.
                  
 Is it a confirmative answer or not?
 ''')
---
+++ experiment/negative.py
@@ -0,0 +1,26 @@
+from finae import ask_llm
+
+# output = ask_llm('''Is Hutao a color or something else?
+
+# Answer yes or no.                 
+# ''')
+# print(output)
+
+# output = ask_llm('''
+# Answer: Blue.
+
+# Is above sentence a confirmative statement?
+# ''')
+# print(output)
+
+# output = ask_llm('''
+# Hutao, a female character from Honkai Impact 3.0, has been created to be of blue and pink color. However, she is not a color or anything else, she is just a character in the game who has blue and pink colors associated with her.
+
+# Is above sentence a confirmative statement?
+# ''')
+# print(output)
+
+output = ask_llm('''
+What is the color of Hutao?
+''')
+print(output)
