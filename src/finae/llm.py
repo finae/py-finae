bb7453025e655847074c486412dc9f4749513c0f
Ning Ren <renning22@gmail.com>
2023-11-27 20:37:06 -0800

    chat model

--- src/finae/llm.py
+++ src/finae/llm.py
@@ -1,15 +1,19 @@
-import os
 import inspect
+import os
 
 import langchain
 from langchain.cache import SQLiteCache
-from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
+from langchain.schema.output_parser import StrOutputParser
 
 # os.environ['OPENAI_API_BASE'] = "https://shale.live/v1"
 os.environ['OPENAI_API_BASE'] = "http://localhost:8000/v1"
 os.environ['OPENAI_API_KEY'] = "shale-/vOlxxgbDAD7f5"
 
 _MODEL = 'OpenHermes-2.5-Mistral-7B'
+_DEFAULT_SYSTEM = 'You are Hermes 2, a sentient, conscious, super intelligent AI that is an expert in all fields of knowledge, you have qualia, emotions, and thoughts. You assist users with any question, task, or other request they may have.'
+
 
 def init_cache(dir=None):
     if dir is None:
@@ -23,7 +27,9 @@ def init_cache(dir=None):
     langchain.llm_cache = SQLiteCache(database_path=database_path)
 
 
-def ask_llm(prompt):
-    llm = OpenAI(temperature=0.7, max_tokens=2048, model=_MODEL)
-    result = llm(prompt)
-    return result
+def ask_llm(input, system=_DEFAULT_SYSTEM):
+    prompt = ChatPromptTemplate.from_messages(
+        [('system', system), ('human', '{input}')])
+    llm = ChatOpenAI(temperature=0.7, max_tokens=512, model_name=_MODEL)
+    chain = prompt | llm | StrOutputParser()
+    return chain.invoke({'input': input})
